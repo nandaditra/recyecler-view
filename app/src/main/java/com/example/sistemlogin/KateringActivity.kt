f6bacefe00273d8233693d1cb217e47f88599dcc
Nanda Aditya Putra <nandaditra56@gmail.com>
2022-11-29 18:50:14 +0700

    add UI + Bakcend recyceler view and data

---
+++ app/src/main/java/com/example/sistemlogin/KateringActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sistemlogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KateringActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_katering)
+    }
+}
