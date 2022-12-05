0d6d7458728bcea8277261f377497b097c0f5277
Nanda Aditya Putra <nandaditra56@gmail.com>
2022-12-05 13:23:58 +0700

    add some code to catch data from Mitra to Katering

--- app/src/main/java/com/example/sistemlogin/KateringActivity.kt
+++ app/src/main/java/com/example/sistemlogin/KateringActivity.kt
@@ -2,12 +2,20 @@ package com.example.sistemlogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class KateringActivity : AppCompatActivity() {
     private lateinit var rvMasakan: RecyclerView
     private var list: ArrayList<Masakan> = arrayListOf()
+
+    companion object {
+        const val EXTRA_NAME = "extra_name"
+        const val EXTRA_DESKRIPSI = "extra_deskripsi"
+        const val EXTRA_PHOTO = "extra_photo"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +26,18 @@ class KateringActivity : AppCompatActivity() {
 
         list.addAll(MasakansData.listData)
         showRecyclerList()
+
+        val tvNamaMitra: TextView = findViewById(R.id.tv_nama_mitra)
+        val tvDeskripsiMitra: TextView = findViewById(R.id.tv_item_deskripsi)
+        val tvPhotoMitra: ImageView = findViewById(R.id.tv_img_photo_mitra)
+
+        val nama = intent.getStringExtra(EXTRA_NAME)
+        val deskripsi = intent.getStringExtra(EXTRA_DESKRIPSI)
+        val photo = intent.getIntExtra(EXTRA_PHOTO, 0)
+
+        tvNamaMitra.text = nama
+        tvDeskripsiMitra.text = deskripsi
+        tvPhotoMitra.setImageResource(photo)
     }
 
     private fun showRecyclerList() {
--- app/src/main/java/com/example/sistemlogin/MainPageActivity.kt
+++ app/src/main/java/com/example/sistemlogin/MainPageActivity.kt
@@ -1,8 +1,10 @@
 package com.example.sistemlogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -26,5 +28,21 @@ class MainPageActivity : AppCompatActivity() {
         val listMitraAdapter = ListMitraAdapter(list)
         rvMitras.adapter = listMitraAdapter
 
+        listMitraAdapter.setOnItemClickCallback(object : ListMitraAdapter.OnItemClickCallback{
+            override fun onItemClicked(data: Mitra) {
+                showSelectorMitra(data)
+            }
+        })
     }
+
+    private fun showSelectorMitra(data: Mitra) {
+        Toast.makeText(this, "You choose "+ data.name, Toast.LENGTH_SHORT).show()
+        var moveKatering = Intent(this@MainPageActivity, KateringActivity::class.java)
+
+        moveKatering.putExtra(KateringActivity.EXTRA_NAME, data.name)
+        moveKatering.putExtra(KateringActivity.EXTRA_PHOTO, data.photo)
+        moveKatering.putExtra(KateringActivity.EXTRA_DESKRIPSI, data.description)
+        startActivity(moveKatering)
+    }
+
 }
